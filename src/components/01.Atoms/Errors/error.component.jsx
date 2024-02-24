b27a5392302193d7d49328d5b65ff0287d68688b
Spatovaliyski <martinspatovaliyski@gmail.com>
2024-02-24 15:11:02 +0200

    Escape the apostrophy in error component

--- src/components/01.Atoms/Errors/error.component.jsx
+++ src/components/01.Atoms/Errors/error.component.jsx
@@ -1,43 +1,42 @@
-import React from 'react'
 import BackArrow from '../BackArrow/back-arrow.component'
 
 const Error = ({ statusCode }) => {
-	if (statusCode === 404) {
-		return (
-			<div>
-				<BackArrow href={'/'} />
-				<h1>404 - Page Not Found</h1>
-				<p>The page you are looking does not exist.</p>
-			</div>
-		)
-	}
+  if (statusCode === 404) {
+    return (
+      <div>
+        <BackArrow href={'/'} />
+        <h1>404 - Page Not Found</h1>
+        <p>The page you are looking does not exist.</p>
+      </div>
+    )
+  }
 
-	if (statusCode === 500) {
-		return (
-			<div>
-				<BackArrow href={'/'} />
-				<h1>500 - Internal Server Error</h1>
-				<p>Sorry, something went wrong.</p>
-			</div>
-		)
-	}
+  if (statusCode === 500) {
+    return (
+      <div>
+        <BackArrow href={'/'} />
+        <h1>500 - Internal Server Error</h1>
+        <p>Sorry, something went wrong.</p>
+      </div>
+    )
+  }
 
-	if (statusCode === 403) {
-		return (
-			<div>
-				<BackArrow href={'/'} />
-				<h1>403 - Forbidden</h1>
-				<p>Sorry, you don't have permission to access this page.</p>
-			</div>
-		)
-	}
+  if (statusCode === 403) {
+    return (
+      <div>
+        <BackArrow href={'/'} />
+        <h1>403 - Forbidden</h1>
+        <p>Sorry, you don&apos;t have permission to access this page.</p>
+      </div>
+    )
+  }
 
-	return (
-		<>
-			<BackArrow href={'/'} />
-			<h1>Something went wrong</h1>
-		</>
-	)
+  return (
+    <>
+      <BackArrow href={'/'} />
+      <h1>Something went wrong</h1>
+    </>
+  )
 }
 
 export default Error;
